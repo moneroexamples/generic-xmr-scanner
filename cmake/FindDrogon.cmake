72516acb6c55797148f962f6096f8852e3c68d0e
moneroexamples <moneroexamples@tuta.io>
2019-04-28 07:39:53 +0800

    add findjsoncpp

--- cmake/FindDrogon.cmake
+++ cmake/FindDrogon.cmake
@@ -7,10 +7,11 @@
 message(STATUS "Searching for Drogon")
 
 add_library(Drogon::Drogon INTERFACE IMPORTED GLOBAL)
+find_package (Jsoncpp REQUIRED)
 
 set_target_properties(Drogon::Drogon PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES 
-      "${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/lib/inc;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/lib/src;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/orm_lib/inc;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/trantor;/usr/include/jsoncpp")
+      "${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/lib/inc;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/lib/src;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/orm_lib/inc;${CMAKE_CURRENT_SOURCE_DIR}/ext/drogon/trantor;${JSONCPP_INCLUDE_DIRS}")
 
 target_link_libraries(Drogon::Drogon INTERFACE
     drogon trantor)
